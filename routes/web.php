475463a5b9e7e6f547ce0214c0c09953c1e0dc09
Pedro Carvalho <pedrosophbc@gmail.com>
2019-09-12 14:48:34 -0300

    Implementando rotas

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,69 @@
 |
 */
 
+                                        // ROTAS COMUNS
+
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/welcome', function(){
+    return "Bem vindo, companheiro!";
+});
+
+Route::get('/welcome/{nome}/{idade}', function($nome, $idade){
+
+    return "Seja bem vindo ".$nome.", você tem ".$idade." anos de idade";
+});
+
+
+                                    // ROTAS COM PARÂMETROS OPCIONAIS
+
+
+Route::get('/busca/{nome?}', function($nome = null){
+    
+    if(isset($nome)){
+        return "Você buscou pelo nome = ".$nome;
+    }else{
+        return "Você não passou o nome como parâmetro";
+    }
+    
+    
+});
+
+
+
+                                       // ROTAS COM REGRAS    
+
+Route::get('/rotacomregra/{nome}/{n}', function($nome, $n){
+
+    for($i=0;$i<$n;$i++){
+        echo $nome." <br>";
+    }
+})->where('n','^[0-9]{1,2}');
+
+                                       // ROTAS COM REGRAS  
+
+Route::prefix('/admin')->group(function(){
+
+    Route::get('/users', function(){
+        return "Admin/users";
+    });
+
+    Route::get('/products', function(){
+        return "Admin/products";
+    });
+    
+    Route::get('/files', function(){
+        return "Admin/files";
+    });
+
+    Route::get('/new/cash/{valor}', function($valor){
+        return "Admin/newCash valor".$valor;
+    });
+
+    Route::get('/new/data/{valor}', function($valor){
+        return "Admin/new Data valor 2 ".$valor;
+    });
 });
